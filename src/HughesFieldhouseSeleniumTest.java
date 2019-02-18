f78e42f24d45dc98fa52744c076c6ae74de9c861
mwoolery <mwoolery@nwmissouri.edu>
2019-02-18 12:12:06 -0600

    added in controller tests for the routes controller

--- src/HughesFieldhouseSeleniumTest.java
+++ src/HughesFieldhouseSeleniumTest.java
@@ -19,17 +19,80 @@ class HughesFieldhouseSeleniumTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mwoolery\\Desktop\\chromedriver.exe");
         browser = new ChromeDriver(options);
 
-        site_header_is_on_home_page(browser);
+        //controller routes test for main pages
+        controller_get_index(browser);
+        controller_get_login(browser);
+        controller_get_awards(browser);
+        controller_get_contact(browser);
+        controller_get_events(browser);
+        controller_get_lockers(browser);
+        controller_get_meetingrooms(browser);
+        //test login
         login_to_admin_page(browser);
         browser.close();
     }
 
     @Test
-    public static void site_header_is_on_home_page(WebDriver browser) {
+    public static void controller_get_index(WebDriver browser) {
         browser.get("http://localhost:3000/");
-        WebElement story = browser.findElement(By.id("story"));
-        assertTrue(story.isDisplayed());
-        System.out.println(story + " - found");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" index found");
+
+    }
+    @Test
+    public static void controller_get_login(WebDriver browser) {
+        browser.get("http://localhost:3000/login");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Login";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" login found");
+
+    }
+    @Test
+    public static void controller_get_awards(WebDriver browser) {
+        browser.get("http://localhost:3000/awards");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Awards";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" awards found");
+
+    }
+    @Test
+    public static void controller_get_contact(WebDriver browser) {
+        browser.get("http://localhost:3000/contact");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Contact";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" contact found");
+
+    }
+    @Test
+    public static void controller_get_events(WebDriver browser) {
+        browser.get("http://localhost:3000/events");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Events";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" events found");
+
+    }
+    @Test
+    public static void controller_get_lockers(WebDriver browser) {
+        browser.get("http://localhost:3000/locker");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Lockers";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" lockers found");
+
+    }
+    @Test
+    public static void controller_get_meetingrooms(WebDriver browser) {
+        browser.get("http://localhost:3000/meetingRooms");
+        String actualTitle = browser.getTitle();
+        String expectedTitle = "Hughes Fieldhouse | Meeting Rooms";
+        assertEquals(expectedTitle,actualTitle);
+        System.out.println(" meeting rooms found");
 
     }
     @Test
